fde9594a5fd3daad9b56656fe561384e1d64b80a
vinoth <vinothvino716@gmail.com>
2023-09-07 18:33:21 +0530

    save the file to local copy

---
+++ src/java_learning/Helloworld.java
@@ -0,0 +1,22 @@
+package java_learning;
+
+public class Helloworld {
+	//This is my first java program
+	/*
+	 * This is my first java program  and its name is Helloworld.java
+	 * 
+	 * To compile a java program,
+	 * javac filename.java
+	 * 
+	 * To execute a java program,
+	 * java filename
+	 */
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			System.out.println("Hello World");
+
+}
+}
